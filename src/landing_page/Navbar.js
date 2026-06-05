9fe94ec1874a8d2009fd90346444d8b5276b43b1
Sazid2006Sheikh <sazid20063008@gmail.com>
2026-06-06 01:44:34 +0530

    Navbar-update

--- src/landing_page/Navbar.js
+++ src/landing_page/Navbar.js
@@ -2,62 +2,190 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function Navbar() {
-  return (
-      <nav class="navbar navbar-expand-lg bg-light border-bottom bg-body-tertiary">
 
-        <div class="container p-2">
-          <Link class="navbar-brand" to={"/"}>
+  return (
+    <>
+
+      <style>
+        {`
+        @keyframes pulse{
+          0%{
+            box-shadow:0 0 0 0 rgba(13,110,253,.5)
+          }
+
+          70%{
+            box-shadow:0 0 0 10px rgba(13,110,253,0)
+          }
+
+          100%{
+            box-shadow:0 0 0 0 rgba(13,110,253,0)
+          }
+        }
+        `}
+      </style>
+
+
+      <nav
+        className="navbar navbar-expand-lg fixed-top"
+        style={{
+          background:"rgba(255,255,255,.9)",
+          backdropFilter:"blur(15px)",
+          boxShadow:"0 5px 25px rgba(0,0,0,.08)"
+        }}
+      >
+
+        <div className="container p-2">
+
+
+          <Link className="navbar-brand" to="/">
+
             <img
               src="media/images/logo.svg"
               alt="Logo"
-              style={{ width: "40%" }}
+              style={{
+                width:"150px"
+              }}
             />
+
           </Link>
+
+
+
           <button
-            class="navbar-toggler"
+
+            className="navbar-toggler"
+
             type="button"
+
             data-bs-toggle="collapse"
+
             data-bs-target="#navbarSupportedContent"
+
             aria-controls="navbarSupportedContent"
+
             aria-expanded="false"
+
             aria-label="Toggle navigation"
+
           >
-            <span class="navbar-toggler-icon"></span>
+
+            <span className="navbar-toggler-icon"></span>
+
           </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <form class="d-flex ms-auto"> 
-              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                  <Link class="nav-link active" aria-current="page" to="/signup">
-                    Signup
-                  </Link>
-                </li>
-                <li class="nav-item">
-                  <Link class="nav-link active" to={"/about"}>
-                    About
-                  </Link>
-                </li>
-                <li class="nav-item">
-                  <Link class="nav-link active"   to={"/products"}>
-                    Product
-                  </Link>
-                </li>
-                <li class="nav-item">
-                  <Link class="nav-link active"  to={"/pricing"}>
-                    Pricing
-                  </Link>
-                </li>
-                <li class="nav-item">
-                  <Link class="nav-link active"   to={"/support"}>
-                    Support
-                  </Link>
-                </li>
-              </ul>
-            </form>
+
+
+
+
+
+          <div
+            className="collapse navbar-collapse"
+            id="navbarSupportedContent"
+          >
+
+
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0 gap-lg-3">
+
+
+              <li className="nav-item">
+
+                <Link className="nav-link active" to="/about">
+
+                  About
+
+                </Link>
+
+              </li>
+
+
+
+              <li className="nav-item">
+
+                <Link className="nav-link active" to="/products">
+
+                  Product
+
+                </Link>
+
+              </li>
+
+
+
+
+              <li className="nav-item">
+
+                <Link className="nav-link active" to="/pricing">
+
+                  Pricing
+
+                </Link>
+
+              </li>
+
+
+
+
+              <li className="nav-item">
+
+                <Link className="nav-link active" to="/support">
+
+                  Support
+
+                </Link>
+
+              </li>
+
+
+
+
+
+              <li className="nav-item">
+
+
+                <Link
+
+                  to="/signup"
+
+                  className="btn btn-primary rounded-pill px-4 ms-lg-3"
+
+                  style={{
+                    animation:"pulse 2s infinite"
+                  }}
+
+                >
+
+                  Signup
+
+                </Link>
+
+
+              </li>
+
+
+            </ul>
+
+
           </div>
+
+
         </div>
+
+
       </nav>
+
+
+    </>
   );
 }
 
+
 export default Navbar;
+
+
+/*
+Add this in index.css:
+
+body{
+  padding-top:75px;
+}
+
+*/
