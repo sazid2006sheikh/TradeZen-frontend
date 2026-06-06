e9c4f8031eab14dfcb6cf3d0d37c4633b0bf0e12
Sazid2006Sheikh <sazid20063008@gmail.com>
2026-06-07 02:17:34 +0530

    style-correction

--- src/landing_page/products/Hero.js
+++ src/landing_page/products/Hero.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 function Hero() {
     return ( 
-    <div className="container mt-5">
-        <div className='text-center mt-5'>
-            <h1>Best in Tech Stack Partnerships</h1>        
+    <div className="container mt-5 ">
+        <div className='text-center p-5 mb-5 mt-5 '>
+            <h1 >Best in Tech Stack Partnerships</h1>        
             <h3 className='text-muted mt-3 fs-4' >Sleek, modern, and intuitive trading platforms</h3>
             <p> Check out our <a href='/investment-offerings' style={{textDecoration:'none'}}> investment offerings <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a></p>
         </div>
