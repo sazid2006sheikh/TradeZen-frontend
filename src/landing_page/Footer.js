100888373df6fc7c389e3cab3596ea6f9e135077
Sazid2006Sheikh <sazid20063008@gmail.com>
2026-06-09 01:00:06 +0530

    UI-correction

--- src/landing_page/Footer.js
+++ src/landing_page/Footer.js
@@ -19,10 +19,6 @@ function Footer() {
                         <br />
                         <a href='/awards' style={{ textDecoration: 'none', color:'dimgray' }}>Awards</a>
                         <br />
-                        <a href='/education' style={{ textDecoration: 'none', color:'dimgray'}}>Education</a>
-                        <br />
-                        <a href='/tradezen-tech' style={{ textDecoration: 'none', color:'dimgray' }}>TradeZen.tech</a>
-                        <br />
                         <a href='/statistics'style={{ textDecoration: 'none', color:'dimgray' }}>Statistics</a>
                     </div>
 
@@ -54,17 +50,7 @@ function Footer() {
                     <p>TradeZen Financial Services: Member of NSE & BSE – SEBI Registration No.: 12IP5A3HO-IO-4A7 | Depository services through TradeZen Securities Pvt. Ltd. – SEBI Registration No.: A3S8F93-N-ID7 | Commodity Trading through TradeZen Commodities Pvt. Ltd. MCX: ABC653 – SEBI Registration No.: 0967VHJN-0A7
                     Registered Address: TradeZen Financial Services, India.
                     For any complaints regarding securities broking, please write to complaint@TradeZen.com, and for DP-related queries, contact TradeZen@hotmail.com. Please ensure you carefully read the Risk Disclosure Document as prescribed by SEBI | ICF.</p>
-                    <p>Procedure to file a complaint on SEBI SCORES: Register on SCORES portal. Mandatory details for filing complaints on SCORES: Name, PAN, Address, Mobile Number, E-mail ID. Benefits: Effective Communication, Speedy redressal of the grievances</p>
-
-                    <p>Investments in securities market are subject to market risks; read all the related documents carefully before investing.</p>
-
-                    <p>Prevent unauthorized transactions in your account. Keep your mobile number and email ID updated with your stock broker to receive transaction alerts directly from the exchange at the end of the trading day. This is issued in the interest of investors.
-
-                    KYC is a one-time process while dealing in the securities market. Once completed through a SEBI-registered intermediary (broker, DP, Mutual Fund, etc.), you do not need to repeat the process when engaging with another intermediary.
-
-                    Dear Investor, if you are subscribing to an IPO, there is no need to issue a cheque. Simply provide your bank account number and sign the IPO application form to authorize your bank for payment in case of allotment. If the allotment is not made, the funds will remain in your bank account.
-
-                    As a business, TradeZen does not provide stock tips and has not authorized anyone to trade on behalf of others. If you encounter anyone claiming to represent TradeZen and offering such services, please report it immediately.</p>
+                    
                 </div>
                 
             </div>
